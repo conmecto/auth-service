a22743903a36b5a4ff45f1d2c1c75e1b2e181901
conmecto <conmecto@gmail.com>
2024-02-04 13:40:31 +0530

    feat: cities cache

--- src/controllers/getCities.ts
+++ src/controllers/getCities.ts
@@ -1,9 +1,15 @@
-import { interfaces, validationSchema, enums } from '../utils';
-import { CustomError, getCitiesByCountry } from '../services';
+import { interfaces, validationSchema } from '../utils';
+import { getCitiesByCountry, cacheClient } from '../services';
 
 const getCities = async (req: interfaces.IRequestObject) => {
     await validationSchema.getCitiesSchema.validateAsync(req.query);
     const { country } = req.query;
+    const cacheCities = await cacheClient.getKey(country?.toLowerCase());
+    if (cacheCities) {
+        return {
+            cities: cacheCities.split(',')
+        }
+    }
     const cities = await getCitiesByCountry(country);
     return {
       cities
