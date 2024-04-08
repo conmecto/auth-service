c1011a91f5520202abed341cef70773d2dbb73c4
conmecto <conmecto@gmail.com>
2024-04-08 14:08:22 +0530

    refactor: login

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -2,46 +2,48 @@ import moment from 'moment';
 import { interfaces, validationSchema, constants, enums } from '../utils';
 import { 
     verifyOtp, CustomError, cacheClient, getUserByNumber, generateAuthToken, updateTokenIdentity, userCreatedMessage,
-    getUserByEmail
+    getUserByEmail, getUserByKey, verifyAppleAuthToken
 } from '../services';
 import updateDeviceInfo from './updateDeviceInfo';
 
 const login = async (req: interfaces.IRequestObject): Promise<interfaces.ILoginUserResponse> => {
     await validationSchema.verifyOtpSchema.validateAsync(req.body);
     const loginUserObject = <interfaces.ILoginUserObject>req.body;
-
-    const user = await getUserByEmail(loginUserObject.email);
+    const user = await getUserByKey('id', Number(loginUserObject.userId));
     if (!user) {
         throw new CustomError(enums.StatusCodes.NOT_FOUND, enums.Errors.USER_NOT_FOUND, enums.ErrorCodes.USER_NOT_FOUND);    
     }
-    if (user.otpValidationAttempts >= constants.OTP_VALIDATION_ATTEMPT_LIMIT) {
-        throw new CustomError(enums.StatusCodes.FORBIDDEN, enums.Errors.OTP_VALIDATION_ATTEMPTS_LIMIT, enums.ErrorCodes.OTP_VALIDATION_ATTEMPTS_LIMIT);    
-    }
-
-    const verifyOtpPayload = {
-        userId: user.userId,
-        code: loginUserObject.code,
-        token: loginUserObject.token,
-        verifyUser: !user.verified
-    }
-    const res = await verifyOtp(verifyOtpPayload);
-    if (!res) {
-        throw new CustomError(enums.StatusCodes.INTERNAL_SERVER, enums.Errors.INTERNAL_SERVER, enums.ErrorCodes.INTERNAL_SERVER);
-    }
-
-    const token = await generateAuthToken({ userId: user.userId });
+    const verifyRes = await verifyAppleAuthToken(req.body.appleAuthToken, <string>user.email, user.appleAuthUserId);
+    if (!verifyRes) {
+        throw new CustomError(enums.StatusCodes.INVALID_TOKEN, enums.Errors.TOKEN_INVALID, enums.ErrorCodes.TOKEN_INVALID);
+    }    
+    // if (user.otpValidationAttempts >= constants.OTP_VALIDATION_ATTEMPT_LIMIT) {
+    //     throw new CustomError(enums.StatusCodes.FORBIDDEN, enums.Errors.OTP_VALIDATION_ATTEMPTS_LIMIT, enums.ErrorCodes.OTP_VALIDATION_ATTEMPTS_LIMIT);    
+    // }
+    // const verifyOtpPayload = {
+    //     userId: user.userId,
+    //     code: loginUserObject.code,
+    //     token: loginUserObject.token,
+    //     verifyUser: !user.verified
+    // }
+    // const res = await verifyOtp(verifyOtpPayload);
+    // if (!res) {
+    //     throw new CustomError(enums.StatusCodes.INTERNAL_SERVER, enums.Errors.INTERNAL_SERVER, enums.ErrorCodes.INTERNAL_SERVER);
+    // }
+    const userId = Number(user.id);
+    const token = await generateAuthToken({ userId });
     const exp = moment().add(token.exp, 'seconds').toISOString(true);
-    await updateTokenIdentity({ userId: user.userId, expiredAt: exp, jti: token.jti });
-    if (!user.verified) {
-        await cacheClient.setKey(`user:${user.userId}`, JSON.stringify({ id: user.userId, verified: true }));
-        await userCreatedMessage(user);
-    }
+    await updateTokenIdentity({ userId, expiredAt: exp, jti: token.jti });
+    // if (!user.verified) {
+    //     await cacheClient.setKey(`user:${userId}`, JSON.stringify({ id: user.userId, verified: true }));
+    //     await userCreatedMessage(user);
+    // }
     if (req.body.deviceToken && req.body.deviceToken !== user.deviceToken) {
-        await updateDeviceInfo(user.userId, req.body.deviceToken);
+        await updateDeviceInfo(userId, req.body.deviceToken);
     }
     return {
         message: constants.USER_LOGGED_IN,
-        data: [{ userId: user.userId, access: token.access, refresh: token.refresh }]
+        data: [{ userId, access: token.access, refresh: token.refresh }]
     }
 }
 
--- src/services/getUserByKey.ts
+++ src/services/getUserByKey.ts
@@ -3,7 +3,7 @@ import { getDbClient } from '../config';
 import { interfaces } from '../utils';
 
 const getUserByKey = async <T>(key: string, value: T): Promise<interfaces.IUserObj | null> => {
-    const query = `SELECT id, verified FROM users WHERE ${key}=$1 AND deleted_at IS NULL`;
+    const query = `SELECT id, verified, email, device_token, apple_auth_user_id FROM users WHERE ${key}=$1 AND deleted_at IS NULL`;
     const params = [value];
     const client = await getDbClient();
     let res: QueryResult | null = null;
@@ -14,7 +14,11 @@ const getUserByKey = async <T>(key: string, value: T): Promise<interfaces.IUserO
     } finally {
         client.release();
     }    
-    return res?.rows?.length ? res.rows[0] : null;
+    return res?.rows?.length ? { 
+        ...res.rows[0], 
+        deviceToken: res.rows[0].device_token,
+        appleAuthUserId: res.rows[0].apple_auth_user_id
+    } : null;
 }
 
 export default getUserByKey;
