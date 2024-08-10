0e9257ec9fbaeca773bd14bd8892c14967b71cf4
conmecto <conmecto@gmail.com>
2024-08-10 18:08:35 +0530

    temp: removed states list

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -28,9 +28,7 @@ const redisClient2 = createClient({
     await redisClient1.connect(); 
     console.log(enums.PrefixesForLogs.REDIS_CONNECTION_READY_CLIENT1 + redisClient1.isReady);
     if (redisClient1.isReady) {
-        const country = 'india';
-        const cities = 'Agartala,Agra,Ahmedabad,Aizawl,Ajmer,Ambala,Amritsar,Andaman and Nicobar Islands,Asansol,Aurangabad,Bahadurgarh,Balasore,Bareilly,Bathinda,Belgavi,Bengaluru,Bhavnagar,Bhilai,Bhopal,Bhubaneswar,Bikaner,Bilaspur,Chamba,Champhai,Chandigarh,Chennai,Chümoukedima,Coimbatore,Cuttack,Dadra and Nagar Haveli and Daman and Diu,Dehradun,Delhi,Dhanbad,Dharamsala,Dharwad,Dibrugarh,Dimapur,Dispur,Durgapur,Faridabad,Gandhinagar,Gangtok,Gaya,Ghaziabad,Gorakhpur,Guntur,Gurugram,Guwahati,Gwalior,Haldwani,Hamirpur,Haridwar,Hisar,Hoshiarpur,Howrah,Hubballi,Hyderabad,Imphal,Indore,Itanagar,Jabalpur,Jaipur,Jaisalmer,Jalandhar,Jalgaon,Jammu & Kashmir,Jamshedpur,Jhansi,Jodhpur,Jorhat,Kakinada,Kannur,Kanpur,Karnal,Kharagpur,Kochi,Kohima,Kolhapur,Kolkata,Kollam,Kota,Kozhikode,Kullu,Kurukshetra,Ladakh,Lakshadweep,Lucknow,Ludhiana,Lunglei,Madurai,Manali,Mandi,Mangaluru,Mapusa,Margao,Mawlai,Meerut,Mokokchung,Mormugao,Mumbai,Mysuru,Nagpur,Namchi,Nashik,Nizamabad,Noida,Palakkad,Panaji,Panipat,Pasighat,Patiala,Patna,Pilani,Prayagraj,Puducherry,Pune,Puri,Raipur,Rajkot,Ranchi,Ravangla,Rishikesh,Rohtak,Roorkee,Ropar,Rourkela,Salem,Shillong,Shimla,Silchar,Siliguri,Solapur,Sonipat,Surat,Tadepalligudem,Tawang,Tezpur,Thiruvananthapuram,Thoubal,Thrissur,Tinsukia,Tiruchirappalli,Tirunelveli,Tirupati,Tura,Udaipur,Udupi,Ujjain,Ukhrul,Vadodara,Varanasi,Vellore,Vijayawada,Vizag,Warangal,Yupia'
-        await redisClient1.set(country, cities);
+//        await redisClient1.set(country, cities);
     }
 })();
 
