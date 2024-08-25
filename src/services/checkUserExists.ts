5307d2b004af4e82c1c2e2b640939488ff1bcbd8
conmecto <conmecto@gmail.com>
2024-08-25 18:01:12 +0530

    fix: user exists

--- src/services/checkUserExists.ts
+++ src/services/checkUserExists.ts
@@ -2,7 +2,15 @@ import { QueryResult } from 'pg';
 import { getDbClient } from '../config';
 
 const checkUserExists = async (appleAuthUserId: number): Promise<boolean> => {
-    const query = 'SELECT id FROM users WHERE apple_auth_user_id=$1 AND deleted_at IS NULL';
+    const query = `
+        SELECT id 
+        FROM users 
+        WHERE 
+        apple_auth_user_id=$1 AND 
+        verified=TRUE AND 
+        terms_accepted=TRUE AND 
+        deleted_at IS NULL
+    `;
     const param = [appleAuthUserId];
     const client = await getDbClient();
     let res: QueryResult | null = null;
